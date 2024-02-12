d08d50dacdec852bbcf40793357c274e3431539e
Charles <159618546+Kyle0443@users.noreply.github.com>
2024-02-12 10:05:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -63,26 +63,26 @@ global.message = {
 },
 
 module.exports = {
-  botname: process.env.BotName || "Queen Anya", 
+  botname: process.env.BotName || "WhizBot", 
   author: process.env.Author || "@PikaBotz",
   packname: process.env.PackName || "Queen Anya v2 MD",
   socialLink: process.env.Web || "https://github.com/PikaBotz",
-  footer: process.env.Footer || "© Queen Anya Bot",
-  prefa: process.env.Prefix || ['-'],
+  footer: process.env.Footer || "© whizBot,
+  prefa: process.env.Prefix || ['.'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Pika~Kun",
-  ownernumber: process.env.Owner_Number || "916900904828",
+  ownername: process.env.Owner_Name || "Kyle😎",
+  ownernumber: process.env.Owner_Number || "254789879395",
   instagramId: process.env.Insta || "8.08_only_mine",
-  warns: process.env.Warns_Limits || 3,
+  warns: process.env.Warns_Limits || 9,
   mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
-  image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
-  image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
-  image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
+  sessionId: process.env.SESSION_ID || "WEF4REdRN0wjSTJqdlE1TmlYNTRMZFl2NGhta_Queen-Anya_nJOdGpGUzgweGFkZmxra_Queen-Anya_0x2RUtSV0IwZw==", 
+  image_1: readFileSync('./1706718897026.jpg'), // Thumbnail for allmenu command
+  image_2: readFileSync('./1706718897026.jpg'), // null image
+  image_3: readFileSync("./1706718897026.jpg"), // Thumbnail for Dashboard
   aliveMedia: readFileSync("./lib/Assets/aliveMedia.mp4"),
   menuMedia: readFileSync('./lib/Assets/menuMedia.mp4'),
   badWords: badWords,
@@ -105,17 +105,17 @@ module.exports = {
 
 
 // Ignore them 👇🏻
-global.botname = process.env.BotName || "Queen Anya" 
+global.botname = process.env.BotName || "whizBot" 
 global.author = process.env.Author || "@PikaBotz" 
 global.packname = process.env.PackName || "Queen Anya v2 MD" 
 global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© Queen Anya Bot" 
+global.footer = process.env.Footer || "© whizBot" 
 global.prefa = process.env.Prefix || ['-'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Pika~Kun" 
-global.ownernumber = process.env.Owner_Number || "916900904828" 
+global.ownername = process.env.Owner_Name || "Kyle😎" 
+global.ownernumber = process.env.Owner_Number || "254789879395" 
 global.adress = process.env.Continent || "Asia, India, Assam" 
-global.timezone = process.env.TimeZone || "Asia/Kolkata" 
+global.timezone = process.env.TimeZone || "Asia/Africa" 
 global.instagramId = process.env.Insta || "8.08_only_mine" 
 global.email = process.env.Email_Id || "example@example.com" 
   
